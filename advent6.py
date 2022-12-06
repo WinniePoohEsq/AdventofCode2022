536c6634cc73750a71a0453a7d6d598593fd3c42
WinniePoohEsq <119713873+WinniePoohEsq@users.noreply.github.com>
2022-12-06 01:25:27 -0600

    Update advent6.py

--- advent6.py
+++ advent6.py
@@ -4,21 +4,20 @@ numbers = handle.read()
 numbers = [i for i in numbers]
 
 
-check_string = [i for i in range(14)]
-counter = 14
+check_string = []
+counter = 4
 for number in numbers:
     while counter > 0:
         check_string.append(number)
-        check_string.remove(check_string[0])
         counter = counter - 1
         break
 
-numbers = numbers[14:]
-index = 13
+numbers = numbers[4:]
+index = 3
 for number in numbers:
     index = index + 1
     y = len(set(check_string))
-    if y < 14:
+    if y < 4:
         check_string.append(number)
         check_string = check_string[1:]
     else:
