dda9e17929b8520fc17e775151f68de4ff288d19
WinniePoohEsq <119713873+WinniePoohEsq@users.noreply.github.com>
2022-12-06 01:19:39 -0600

    Update advent6.py

--- advent6.py
+++ advent6.py
@@ -6,34 +6,19 @@ handle = open(name)
 numbers = handle.read()
 numbers = [i for i in numbers]
 
-letters_value = [i for i in range(26)]
-alphabet = (string.ascii_lowercase)
-alphabet = [i for i in alphabet]
 
-for value in reversed(letters_value):
-    location = value
-    alphabet.insert(location, value)
-
-working_list = []
-for number in numbers:
-    location = alphabet.index(number)
-    value = alphabet[location - 1]
-    working_list.append(value)
-
-#change range, counter for second part
 checkstring = [i for i in range(14)]
 counter = 14
-for number in working_list:
+for number in numbers:
     while counter > 0:
         checkstring.append(number)
         checkstring.remove(checkstring[0])
         counter = counter - 1
         break
 
-#change range, index, y for second part
-working_list = working_list[14:]
+numbers = numbers[14:]
 index = 13
-for number in working_list:
+for number in numbers:
     index = index +  1
     y = len(set(checkstring))
     if y < 14:
